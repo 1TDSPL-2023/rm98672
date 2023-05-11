976f73c204e60dea0d1345295f92117fee47c60f
M1relly <rm98672@fiap.com.br>
2023-05-11 08:33:30 -0300

    Alteração do exercício

--- exercicio/exercicio-1.js
+++ exercicio/exercicio-1.js
@@ -1,7 +1,9 @@
+// EXERCÍCIO 1 - mude a cor de fundo
 function changeBackgroundColor() {
     document.body.style.backgroundColor = "lightgray";
   }
   
+// EXERCÍCIO 2 - adicione um item à lista
   function addItem() {
     event.preventDefault();
     var newItem = document.createElement('li');
@@ -12,7 +14,7 @@ function changeBackgroundColor() {
     document.getElementById('newItem').value = "";
   }
   
-  
+// EXERCÍCIO 3 - altere a imagem
   function changeImage() {
     var image = document.getElementById('Imagem');
     if (image.src.match("lfechado")) {
@@ -20,11 +22,12 @@ function changeBackgroundColor() {
     } 
   }
 
+// EXERCÍCIO 4 - mostre uma mensagem
   function displayMessage() {
     document.getElementById('menssagem').innerHTML = "Sabe onde qual é a fonte mágica?";
   }
   
-
+// EXERCÍCIO 5 - Esconda um elemento 
   function hideElement() {
     var element = document.getElementById('elemento');
     if (element.style.display === "none") {
