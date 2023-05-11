2af6b0025e9b43f1d8cc1e90dfd8bf4c7a648c34
M1relly <rm98672@fiap.com.br>
2023-05-11 08:38:28 -0300

    Alteração do exercício

--- exercicio/exercicio-1.js
+++ exercicio/exercicio-1.js
@@ -1,35 +1,38 @@
+// EXERCÍCIO 1 - mude a cor de fundo
 function changeBackgroundColor() {
-    document.body.style.backgroundColor = "lightgray";
-  }
-  
-  function addItem() {
-    event.preventDefault();
-    var newItem = document.createElement('li');
-    var inputValue = document.getElementById('newItem').value;
-    var textNode = document.createTextNode(inputValue);
-    newItem.appendChild(textNode);
-    document.getElementById('list').appendChild(newItem);
-    document.getElementById('newItem').value = "";
-  }
-  
-  
-  function changeImage() {
-    var image = document.getElementById('Imagem');
-    if (image.src.match("lfechado")) {
-      image.src = "./img/laberto.jpg";
-    } 
-  }
+  document.body.style.backgroundColor = "lightgray";
+}
 
-  function displayMessage() {
-    document.getElementById('menssagem').innerHTML = "Sabe onde qual é a fonte mágica?";
-  }
-  
+// EXERCÍCIO 2 - adicione um item à lista
+function addItem() {
+  event.preventDefault();
+  var newItem = document.createElement('li');
+  var inputValue = document.getElementById('newItem').value;
+  var textNode = document.createTextNode(inputValue);
+  newItem.appendChild(textNode);
+  document.getElementById('list').appendChild(newItem);
+  document.getElementById('newItem').value = "";
+}
 
-  function hideElement() {
-    var element = document.getElementById('elemento');
-    if (element.style.display === "none") {
-      element.style.display = "block";
-    } else {
-      element.style.display = "none";
-    }
+// EXERCÍCIO 3 - altere a imagem
+function changeImage() {
+  var image = document.getElementById('Imagem');
+  if (image.src.match("lfechado")) {
+    image.src = "./img/laberto.jpg";
+  } 
+}
+
+// EXERCÍCIO 4 - mostre uma mensagem
+function displayMessage() {
+  document.getElementById('menssagem').innerHTML = "Sabe onde qual é a fonte mágica?";
+}
+
+// EXERCÍCIO 5 - Esconda um elemento 
+function hideElement() {
+  var element = document.getElementById('elemento');
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
   }
+}
